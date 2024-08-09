548cb50ad7cac76bc1138f104f4d0bce5958e91d
wallacevieira <wallaceofc@hotmail.com>
2024-08-09 12:44:48 -0300

    feat: desafio

---
+++ examples/script.js
@@ -0,0 +1,111 @@
+/*
+const cars = ["Saab", "Volvo", "BMW"];
+// const cars = new Array("Saab", "Volvo", BMW);
+console.log(cars[0]);
+*/
+
+/* 
+var frutas = [];
+frutas.push("banana", "maçã", "Pêssego");
+console.log(frutas.length);
+*/
+
+/* 
+const cars = ["Saab", "Volvo", "BMW"];
+let car = cars[0];
+*/
+
+/* 
+const cars = ["Saab", "Volvo", "BMW"];
+cars[0] = "Opel";
+console.log(cars);
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const fruitsString = fruits.toString();
+console.log(fruitsString);
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fLen = fruits.length;
+
+let text = "<ul>";
+for (let i = 0; i < fLen; i++) {
+  text += "<li>" + fruits[i] + "</li>";
+}
+
+text += "</ul>";
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Apple"];
+fruits.push("Lemon");
+console.log(fruits);
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const newArray = fruits.join(" * ");
+console.log(newArray);
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.pop();
+console.log(fruits);
+let fruit = fruits.pop();
+console.log(fruit);
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let len = fruits.push("kiwi");
+console.log(fruits);
+console.log(len);
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let len = fruits.shift();
+console.log(fruits);
+console.log(len);
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let len = fruits.unshift("Lemon");
+console.log(fruits);
+console.log(len);
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruits);
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(0, 1);
+console.log(fruits);
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+delete fruits[0];
+console.log(fruits);
+*/
+
+/* 
+const arr = [[1, 2], [3, 4], [5, 6]];
+const newArr = arr.flat();
+console.log(newArr);
+*/
+
+/* 
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const fruit = fruits.slice(1);
+console.log(fruit);
+*/
