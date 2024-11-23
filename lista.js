217169e6f8bef177041644700f3b5a3194a4f4a0
wallace027dev <wallaceofc@hotmail.com>
2024-11-23 09:58:47 -0300

    exemplo 1

---
+++ lista.js
@@ -0,0 +1,18 @@
+// Primeira forma
+const nomes = ["Josué", "Junior"];
+
+// Segunda forma
+const notas = [];
+notas[0] = 8;
+notas[1] = 9;
+
+// Terceira forma
+const carros = new Array("BYD", "Toyota", "Chevrolet")
+
+// Exemplo: criar lista de frutas
+
+const frutas = ["maçã", "banana", "pera"];
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
